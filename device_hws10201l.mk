b860d39686e11fe46bd901b8efdb8a4c51a53e19
Anton <towa48@gmail.com>
2016-10-23 21:13:53 +0300

    Modify device and system properties;

--- device_hws10201l.mk
+++ device_hws10201l.mk
@@ -1,12 +1,32 @@
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/huawei/hws10201l/hws10201l-vendor.mk)
+
+PRODUCT_AAPT_CONFIG := xlarge mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 DEVICE_PACKAGE_OVERLAYS += device/huawei/hws10201l/overlay
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=30 \
+    tf.enable=y \
+    drm.service.enabled=true
+
+include frameworks/native/build/tablet-dalvik-heap.mk
+
+PRODUCT_CHARACTERISTICS := tablet
+
+# Permissions
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+#    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+#    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+#    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+# Bootanimation
+PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/800.zip
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/huawei/hws10201l/kernel
@@ -20,5 +40,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_hws10201l
+PRODUCT_NAME := cm_hws10201l
 PRODUCT_DEVICE := hws10201l
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo cm_hws10201l-userdebug
+add_lunch_combo cm_hws10201l-eng
+add_lunch_combo cm_hws10201l-user
